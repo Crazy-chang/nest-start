02f91852ce6cf5fe6c1cfa33627cab77d3e43064
zhangzc <zhangzc@example.com>
2024-04-11 14:27:06 +0800

    数据库配置数据抽离单独配置文件

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,34 +4,41 @@ import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { TestModule } from './modules/test/test.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getConfig } from './core/common/index';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    // 配置模块
     ConfigModule.forRoot({
       ignoreEnvFile: true,
+      isGlobal: true,
       load: [getConfig],
     }),
     AuthModule,
     UserModule,
     TestModule,
+    // 连接数据库
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql', // 数据库类型
-        host: '', // 主机，默认为localhost
-        port: 3306, // 端口号
-        username: 'prod', // 用户名
-        password: 'Bojia2023', // 密码
-        database: 'zzc', // 数据库名
-        logging: true, // 开启所有数据库信息打印
-        // timezone: '+08:00', // 服务器上配置的时区
-        timezone: 'Z',
-        synchronize: true, // 根据实体自动创建数据库表，生产环境建议关闭，否则可能导致数据被清空
-        // entities: [`${__dirname}/**/*.entity{.ts,.js}`], // 引入实体，使用该方式可能有bug   mac 系统支持这种
-        autoLoadEntities: true, // 自动加载实体 // windows 系统支持这种
-      }),
+      useFactory: (configService: ConfigService) => {
+        const { host, port, username, password, database } = configService.get('DB');
+        return {
+          type: 'mysql', // 数据库类型
+          host, // 主机，默认为localhost
+          port, // 端口号
+          username, // 用户名
+          password, // 密码
+          database, // 数据库名
+          logging: true, // 开启所有数据库信息打印
+          // timezone: '+08:00', // 服务器上配置的时区
+          timezone: 'Z',
+          synchronize: false, // 根据实体自动创建数据库表，生产环境建议关闭，否则可能导致数据被清空
+          // entities: [`${__dirname}/**/*.entity{.ts,.js}`], // 引入实体，使用该方式可能有bug   mac 系统支持这种
+          autoLoadEntities: true, // 自动加载实体 // windows 系统支持这种
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
--- src/core/common/index.ts
+++ src/core/common/index.ts
@@ -1,15 +1,22 @@
 import * as dayjs from 'dayjs';
 import { ValueTransformer } from 'typeorm';
 
+import * as fs from 'fs';
+import * as path from 'path';
+import { parse } from 'yaml';
+
 // 读取项目配置
 export const getConfig = () => {
-  const config = {
-    port: parseInt(process.env.PORT, 10) || 3000,
-    database: {
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    },
-  };
+  const yamlPath = path.join(process.cwd(), `/config/env.yaml`);
+  const file = fs.readFileSync(yamlPath, 'utf8');
+  const config = parse(file);
+  // const config = {
+  //   port: parseInt(process.env.PORT, 10) || 3000,
+  //   database: {
+  //     host: process.env.DATABASE_HOST,
+  //     port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  //   },
+  // };
 
   return config;
 };
